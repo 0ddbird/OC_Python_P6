7f175f7d74c13e43a91d51720ab17f76b0688e0c
0ddbird <giacomo.llinares@gmail.com>
2023-01-08 17:33:36 +0100

    FEAT : implemented the event listeners to toggle the modal

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log('test');
+const infoButtons = document.querySelectorAll('.info-btn');
+const modal = document.querySelector('#modal-background');
+console.log(modal);
+const modalCloseButton = document.querySelector('#modal_close-btn');
+const displayModal = () => {
+    if (modal != null) {
+        modal.classList.remove('hidden');
+        modal.classList.add('displayed');
+    }
+};
+const hideModal = () => {
+    if (modal != null) {
+        modal.classList.remove('displayed');
+        modal.classList.add('hidden');
+    }
+};
+infoButtons.forEach(button => {
+    button.addEventListener('click', displayModal);
+});
+if (modalCloseButton != null)
+    modalCloseButton.addEventListener('click', hideModal);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('test')
+const infoButtons = document.querySelectorAll('.info-btn')
+const modal = document.querySelector('#modal-background')
+console.log(modal)
+const modalCloseButton = document.querySelector('#modal_close-btn')
+const displayModal = (): void => {
+  if (modal != null) {
+    modal.classList.remove('hidden')
+    modal.classList.add('displayed')
+  }
+}
+
+const hideModal = (): void => {
+  if (modal != null) {
+    modal.classList.remove('displayed')
+    modal.classList.add('hidden')
+  }
+}
+
+infoButtons.forEach(button => {
+  button.addEventListener('click', displayModal)
+})
+
+if (modalCloseButton != null) modalCloseButton.addEventListener('click', hideModal)
